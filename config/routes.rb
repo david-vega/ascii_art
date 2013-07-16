a506b886c731de48e374d7356faf56ed92e6ea2b
David Vega <zetacu@gmail.com>
2013-07-16 10:03:09 -0700

    WIP twitter auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 AsciiArt::Application.routes.draw do
+  match "/auth/:provider/callback" => "sessions#create"
+  match "/signout" => "sessions#destroy", :as => :signout
+
   resources :users
   resources :posts
 
